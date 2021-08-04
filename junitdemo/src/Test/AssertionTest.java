f6bdb0d60dd8323eebe3323f2ea7c72e4af7fe08
Mayank <mayankrajput554@gmail.com>
2021-08-04 16:52:52 +0530

    junit commit 1

---
+++ junitdemo/src/Test/AssertionTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class AssertionTest {
+
+	
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ junitdemo/src/Test/EmployeeTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
